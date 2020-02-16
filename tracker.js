d8300e29012d3e9aa52e0c1c638d4f11b1487804
drthisguy <page.c.tyler@gmail.com>
2020-02-16 12:26:09 -0500

    added functions to view 3 main databases as table in the console.

--- tracker.js
+++ tracker.js
@@ -1,14 +1,40 @@
 const mysql = require('mysql'),
-    Prompt = require('./lib/Prompts');
+    Prompt = require('./lib/Prompts'),
+    cTable = require('console.table');
     
 const prompt = new Prompt,
  connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    password:"taytum123",
-    database: "tracker_db"
+    user: "root",
+    password:"YourRootPassword",
+    database: "tracker_DB"
 });
 
 connection.connect( err => {
     if (err) throw err;
 });
+
+function viewDepartments() {
+    connection.query("SELECT * FROM departments", (err, res) => {
+      if (err) throw err;
+  
+      console.table(res);
+    });
+  }
+
+ function viewEmployees() {
+    connection.query("SELECT * FROM employees", (err, res) => {
+    if (err) throw err;
+
+    console.table(res);
+ });
+}
+
+ function viewRoles() {
+    connection.query("SELECT * FROM roles", (err, res) => {
+    if (err) throw err;
+
+    console.table(res);
+ });
+}
