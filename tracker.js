4376eb7a2297a1c1744b67a39438fe3bdb941096
drthisguy <page.c.tyler@gmail.com>
2020-02-20 03:32:13 -0500

    moved employee editor and options to own class module. debugged that.

--- tracker.js
+++ tracker.js
@@ -1,180 +1,36 @@
-        const UI = require('./lib/Prompts'),
-         SQL = require('./lib/sql'),
-         Dept = require('./lib/depts'),
-         cTable = require('console.table'),
+        const UI = require('./lib/ui'),
+           Dept = require('./lib/depts'),
+           Employ = require('./lib/employees'),
 
   ui = new UI,
   dept = new Dept,
-  sql = new SQL;
+  emp = new Employ;
 
-(function start() { 
-    ui.start().then(({ catagory }) => {
+(function start() {
 
-    switch (catagory) {
-        case 'Employees':
-            employeeManager();
-            break;
+  const scope = () => start();
 
-        case 'Departments':
-            dept.manager();
-            break;
+  ui.start()
+    .then(({ catagory }) => {
+      switch (catagory) {
+        case "Employees":
+          emp.manager(scope);
+          break;
 
-        case 'Job Titles':
-            roleManager();
-            break;
+        case "Departments":
+          dept.manager(scope);
+          break;
+
+        case "Job Titles":
+          roleManager(scope);
+          break;
+
         default:
-        // Quit program
-        sql.end();
-        console.log('Have a great day!');
-        process.exit([0]);
-    }
-}).catch( err => console.log(err));
+          // Quit program
+          sql.end();
+          console.log("Have a great day!");
+          process.exit([0]);
+      }
+    })
+    .catch(err => console.log(err));
 })();
-
-function employeeManager() {
-
-ui.employeeOpts().then(async ({ option }) => {
-    
-    switch (option) {
-        case 'View all employees':
-            console.clear();
-            sql.viewEmployees();
-            employeeManager();
-            break;
-
-        case 'Update an employee':
-            const { id } = await ui.getId();
-            let employee = await findEmployeeById(id, 'arg');
-            ui.update().then(({ edit }) => {
-                employeeEditor(employee, edit)
-                .then( employee => {
-                console.table(employee);
-                employeeManager();
-             }
-            )}).catch( err => console.log(err));
-            break;
-
-        case 'Add a new employee':
-            ui.newEmployee().then( answers => {
-                sql.addEmployee(answers);
-                employeeManager();
-            }).catch( err => console.log(err));
-            break;
-
-        case 'Find employees by their manager':
-            ui.getManager().then(async ({ id }) => {
-                await sql.lookUpByManager(id);
-                employeeManager();
-                
-            }).catch( err => console.log(err));
-            break;
-
-        case 'Look up employee by name':
-            ui.getName().then(async ({ name }) => {
-                await sql.lookUpByName(name);
-                employeeManager();
-                
-            }).catch( err => console.log(err));
-            break;
-
-        case 'Look up employee by ID':
-            ui.getId().then(({ id }) => {
-                findEmployeeById(id);
-
-            }).catch( err => console.log(err));
-            break;
-
-        case 'Terminate an employee':
-            ui.getId().then(async ({ id }) => {
-                const employee = await findEmployeeById(id, 'arg')
-                ui.confirm(employee).then(({ confirm }) => {
-                    if(confirm) {
-                        sql.terminate(employee);
-                        employeeManager();    
-                    } else {
-                        employeeManager();
-                    }
-                    }).catch( err => console.log(err));
-                 })
-             break;
-        default:
-            //returns to the home screen
-            start();
-    }
-  }).catch( err => console.log(err));
-}
-
-function employeeEditor(employee, editType) {
-return new Promise((resolve, reject) => {
-
-   try { 
-    switch (editType) {
-        case 'First Name':
-            ui.getNewFirst().then( ({ name }) => {
-                employee.first_name = name;
-                sql.updateFirstName(employee);
-                resolve(employee);
-            }).catch(err => {if (err) throw err});
-            break;
-
-        case 'Last Name':
-            ui.getNewLast().then( ({ name }) => {
-                employee.last_name = name;
-                sql.updateLastName(employee);
-                resolve(employee);
-            }).catch(err => {if (err) throw err});
-            break;
-
-        case 'Department':
-            ui.getNewDeptId().then( ({ dept }) => {
-                employee.dept_id = dept;
-                sql.setNewDept(employee);
-                resolve(employee);
-            }).catch(err => {if (err) throw err});
-            break;
-
-        case 'Role':
-            ui.getNewRole().then( ({ role }) => {
-                employee.role_id = role;
-                sql.setNewRole(employee);
-                resolve(employee);
-            }).catch(err => {if (err) throw err});
-            break;
-
-        case 'Manager':
-            ui.getNewManager().then( ({ manager }) => {
-                employee.manager_id = manager;
-            }).catch(err => {if (err) throw err});
-            resolve(employee);
-            return employee;
-        default:
-            employeeOpts();
-    }
-    }  catch(err) {reject(console.log((err)))};
- })
-}
-
-function findEmployeeById(id, ...args) {
-    return new Promise(async resolve => {
-
-        const args = Array.from(arguments),
-            res = await sql.lookUpByID(id);
-
-        if (!res) {
-            console.log('\nEmployee not found\n');
-            employeeManager();
-
-        } else if (args.length > 1) {
-            const [ employee ] = res;
-            console.clear();
-            console.log('\n');
-            console.log(`${console.table(`You have selected:`, res)}`);
-            resolve(employee);
-
-        } else {
-            console.log('\n');
-            console.log(`${console.table(`Employee with ID: ${id}`, res)}`);
-            employeeManager();
-        }
- });
-}
