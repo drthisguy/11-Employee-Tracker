f3144c7111151e518dfe2b0b121bceb4cf9e9b66
drthisguy <page.c.tyler@gmail.com>
2020-02-21 12:00:07 -0500

    dropped manager table and just set them according to dept. resolved some of the bug introduced by this change.

--- schema.sql
+++ schema.sql
@@ -8,6 +8,7 @@ USE tracker_DB;
 CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT,
     department VARCHAR(50) NOT NULL,
+    manager VARCHAR(50) NULL,
     PRIMARY KEY (id)
 );
 
@@ -20,16 +21,6 @@ CREATE TABLE roles (
     FOREIGN KEY (dept_id) REFERENCES departments(id)
 );
 
-CREATE TABLE managers (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    dept_id INT NULL,
-    role_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (dept_id) REFERENCES departments(id)
-);
-
 
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT,
@@ -37,11 +28,9 @@ CREATE TABLE employees (
     last_name VARCHAR(30) NOT NULL,
     dept_id INT NULL,
     role_id INT NULL,
-    manager_id INT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (dept_id) REFERENCES departments(id),
-    FOREIGN KEY (manager_id) REFERENCES managers(id)
+    FOREIGN KEY (dept_id) REFERENCES departments(id)
 );
 
 
--- seeds.sql
+++ seeds.sql
@@ -1,15 +1,12 @@
 /*Seeds for MySQL tables*/
 USE tracker_DB;
 
-INSERT INTO departments (department)
-VALUES ('Engineering'), ('Purchassing'), ('Production'), ('Marketing'), ('Quality Control'), ('Tech Support'), ('Sales'), ('Human Resources'), ('Owner'), ('Management');
+INSERT INTO departments (department, manager)
+VALUES ('Engineering', 'Ernie'), ('Purchassing', 'Jessica'), ('Production', 'Carrie'), ('Marketing', 'Bart'), ('Quality Control', 'Jared'), ('Tech Support', 'Noah'), ('Sales', 'Samantha'), ('Human Resources', 'Jessica'), ('Owner', NULL), ('Management', NULL);
 
 INSERT INTO roles (title, dept_id, salary)
 VALUES ('Owner', 9, 0), ('Manager', 9, 100000.00), ('Engineer I', 1, 77400.00),  ('Engineer II', 1, 83800.00), ('Engineer III', 1, 92900.00), ('Technician I', 6, 41000.00), ('Technician II', 1, 47300.00), ('Graphic Desiner', 4, 49600.00), ('Receptionist', 8, 35000.00), ('Sales Rep', 7, 25800.00), ('Maketing Rep', 4, 45400.00), ('IT Specialist', 6,  46500.00), ('Production Assembler', 2, 31200.00), ('Logistics Specialist', 2, 45200.00), ('Intern', 8, 0);
 
-INSERT INTO managers (name, dept_id, role_id)
-VALUES ('Ernie', 1, 2), ('Jessica', 2, 2),  ('Carrie', 3, 2), ('Bart', 4, 2), ('Jared', 5, 2), ('Noah', 6, 2), ('Samantha', 7, 2), ('Elizabeth', 8, 2);
-
-INSERT INTO employees (first_name, last_name, dept_id, role_id, manager_id)
-VALUES ('John', 'Doe',9, 1, 1), ('Jeff', 'Bezos',8, 16, 2), ('Trisia', 'Harper', 1, 3, 1), ('Penny', 'Warren', 8, 9, 8), ('Keith', 'Brock', 7, 6, 1), ('Alexis', 'Walters', 1, 5, 1), ('Stella', 'Reid', 4, 11, 4), ('David', 'Sommers', 5, 7, 6), ('Ginger', 'Ortiz', 7, 12, 4), ('Madeline', 'Green', 2, 15, 2), ('Julio', 'Carlson' ,1 , 4, 1), ('Ada', 'Flowers', 7, 13, 4), ('Jane', 'Doe', 8, 10, 8), ('Allison', 'Lopez', 4, 8, 4), ('Howard', 'Burke', 3, 14, 3), ('Doctor', 'Who', 3, 14, 3);
+INSERT INTO employees (first_name, last_name, dept_id, role_id)
+VALUES ('John', 'Doe',9, 1), ('Jeff', 'Bezos',8, 4), ('Trisia', 'Harper', 1, 3), ('Penny', 'Warren', 8, 9), ('Keith', 'Brock', 7, 6), ('Alexis', 'Walters', 1, 5), ('Stella', 'Reid', 4, 11), ('David', 'Sommers', 5, 7), ('Ginger', 'Ortiz', 7, 12), ('Madeline', 'Green', 2, 15), ('Julio', 'Carlson' ,1 , 4), ('Ada', 'Flowers', 7, 13), ('Jane', 'Doe', 8, 10), ('Allison', 'Lopez', 4, 8), ('Howard', 'Burke', 3, 14), ('Doctor', 'Who', 3, 14), ('Ernie', 'Bighead', 1, 2), ('Jessica', 'Trumpster', 2, 2),  ('Carrie', 'Fisher', 3, 2), ('Bart', 'Simpson', 4, 2), ('Jared', 'Wiener', 5, 2), ('Noah', 'Goodman', 6, 2), ('Samantha','Fox', 7, 2), ('Elizabeth', 'Warren', 8, 2);
 
